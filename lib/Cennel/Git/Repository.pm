bf6a5489add2ff2b876a24cc5919134032df9cae
Wakaba <wakabatan@hatena.ne.jp>
2012-10-15 21:35:17 +0900

    disable prompts

--- lib/Cennel/Git/Repository.pm
+++ lib/Cennel/Git/Repository.pm
@@ -51,6 +51,7 @@ sub run_repo_command_as_cv {
                     $task_name,
                     $json_f,
                 ],
+                '<' => '/dev/null',
                 '>' => sub { $self->print_message($_[0]) if defined $_[0] },
                 '2>' => sub { $self->print_message($_[0]) if defined $_[0] },
             )->cb(sub {
