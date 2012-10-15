cfd169a26aee65e3fcf3e318ef05db53be72c7d5
Wakaba <wakabatan@hatena.ne.jp>
2012-10-15 16:09:05 +0900

    debug

--- config/service.in/cenneld/bin.sh
+++ config/service.in/cenneld/bin.sh
@@ -4,4 +4,5 @@ export MYSQL_DSNS_JSON=@@INSTANCECONFIG@@/dsns.json
 export KARASUMA_CONFIG_JSON=@@INSTANCECONFIG@@/@@INSTANCENAME@@.json
 export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
 export WEBUA_DEBUG=2
+export SQL_DEBUG=1
 exec setuidgid @@USER@@ @@ROOT@@/perl @@ROOT@@/bin/runner.pl
