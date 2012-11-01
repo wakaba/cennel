9e9c1fdfc6be12d75e1a9fec1ebf8c2517511e3e
Wakaba <wakabatan@hatena.ne.jp>
2012-11-01 21:32:33 +0900

    envs

--- config/service.in/cenneld/bin.sh
+++ config/service.in/cenneld/bin.sh
@@ -3,6 +3,8 @@ exec 2>&1
 export MYSQL_DSNS_JSON=@@INSTANCECONFIG@@/dsns.json
 export KARASUMA_CONFIG_JSON=@@INSTANCECONFIG@@/@@INSTANCENAME@@.json
 export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
-export WEBUA_DEBUG=2
-export SQL_DEBUG=1
+
+export SQL_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.SQL_DEBUG text`
+export WEBUA_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.WEBUA_DEBUG text`
+
 exec setuidgid @@USER@@ @@ROOT@@/perl @@ROOT@@/bin/runner.pl
