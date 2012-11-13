afdb484dd1d4b27d73e29be645ad1896c3417a77
Wakaba <wakabatan@hatena.ne.jp>
2012-11-13 15:50:25 +0900

    home

--- config/service.in/cenneld/bin.sh
+++ config/service.in/cenneld/bin.sh
@@ -3,6 +3,7 @@ exec 2>&1
 export MYSQL_DSNS_JSON=@@INSTANCECONFIG@@/dsns.json
 export KARASUMA_CONFIG_JSON=@@INSTANCECONFIG@@/@@INSTANCENAME@@.json
 export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
+export HOME=@@LOCAL@@/home
 
 export SQL_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.SQL_DEBUG text`
 export WEBUA_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.WEBUA_DEBUG text`
